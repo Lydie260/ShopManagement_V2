a92529f058f8255079abb15bebf4a568a9062ac2
Lydie <gatarelydie370@gmail.com>
2022-11-11 15:33:53 +0200

    added product

--- backend/src/utils/hashPassword.js
+++ backend/src/utils/hashPassword.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import "dotenv/config";
 
 export const hashPassword = (password) => {
-  return bcrypt.hashSync(password,10);
+  return bcrypt.hashSync(password, 10);
 };
 
 export const isPasswordMatching = (password, hashedPassword) => {
